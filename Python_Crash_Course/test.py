99fbbbed985cf882b5727c1b03dd1f6ee83c033d
SodaCode <dominicsaechao1@gmail.com>
2023-10-16 00:51:51 -0700

    test file updated

--- Python_Crash_Course/test.py
+++ Python_Crash_Course/test.py
@@ -1,5 +1,28 @@
-pets = ["cat", "dog", "turtle"]
+users = {
+    'dsaechao': {
+        'first_name': 'Dominic',
+        'last_name': 'Saechao',
+        'age': 27,
+        'location': 'SJ'
+    },
+    'jchan': {
+        'first_name': 'Josephine',
+        'last_name': 'Chan',
+        'age': 28,
+        'location': 'SJ'
+    },
+    'aflores': {
+        'first_name': 'Alan',
+        'last_name': 'Flores',
+        'age': 27,
+        'location': 'SAC'
+    },
+}
 
-for pet in pets:
-    print(f"A {pet} would make a great pet!")
-print("All these animals would make a great pets because they have four legs!")
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first_name']} {user_info['last_name']}"
+    location = user_info['location']
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location}")
